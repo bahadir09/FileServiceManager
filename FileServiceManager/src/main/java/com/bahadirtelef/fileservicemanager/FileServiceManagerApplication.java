a6aa391c342740492c3f22111915354f0eb476a2
bahadir09 <bahadirtelef9@gmail.com>
2023-07-19 00:29:14 +0300

    proje_start

---
+++ FileServiceManager/src/main/java/com/bahadirtelef/fileservicemanager/FileServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bahadirtelef.fileservicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceManagerApplication.class, args);
+    }
+
+}
---
+++ FileServiceManager/src/test/java/com/bahadirtelef/fileservicemanager/FileServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadirtelef.fileservicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
