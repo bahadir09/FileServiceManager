c0c917938a74c4eb70b20c3fa0939698808a2ef8
bahadir09 <bahadirtelef9@gmail.com>
2023-07-19 20:50:08 +0300

    Fix-JWT_TOKEN_VALIDITY
    Token süresi 5 dk olarak güncellendi

--- FileServiceManager/src/main/java/com/bahadirtelef/fileservicemanager/security/JwtTokenUtil.java
+++ FileServiceManager/src/main/java/com/bahadirtelef/fileservicemanager/security/JwtTokenUtil.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 public class JwtTokenUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
-    public static final long JWT_TOKEN_VALIDITY = 1 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60;
     SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     //retrieve username from jwt token
